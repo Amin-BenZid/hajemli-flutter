a5842c3561a7d74db6aa9dedf9490bc05f306815
tfk-miza <mazensahloulitb@gmail.com>
2024-10-24 17:20:25 +0100

    1st

--- lib/views/mainScreen.dart
+++ lib/views/mainScreen.dart
@@ -7,15 +7,55 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffEEEEEE),
-          body: Column(
-            children: [
-              const SizedBox(height: 50,),
-              const Center(child: Image(image: AssetImage("lib/assets/2.png"),height: 276,width: 276,)),
-              const SizedBox(height: 20,),
-              Container(width: 332,height: 301,color: const Color(0xff333333),)
-            ],
+      body: Column(
+        children: [
+          const SizedBox(height: 50),
+          const Center(
+            child: Image(
+              image: AssetImage("lib/assets/2.png"),
+              height: 276,
+              width: 276,
+            ),
           ),
+          const SizedBox(height: 20),
 
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Container(
+              width: 332,
+              height: 301,
+              decoration: BoxDecoration(
+                color: const Color(0xff333333),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text('Button 1'),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text('Button 2'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

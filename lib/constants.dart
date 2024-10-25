9b2971bd358f3f016a0e14a88582120accef9aef
tfk-miza <mazensahloulitb@gmail.com>
2024-10-25 12:12:46 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+const bg_color = Color(0xffEEEEEE);
+const primary_color = Color(0xff93833D);
+const secondary_color = Color(0xff333333);
